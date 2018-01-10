91bb8f28ca092f5aef957f7852a788196ac88e1d
Clayton Auld <clayauld@gmail.com>
2018-01-10 12:23:37 -0600

    Initial creation of language files

---
+++ spec/language-hart-eddl-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageHartEddlView from '../lib/language-hart-eddl-view';
+
+describe('LanguageHartEddlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
